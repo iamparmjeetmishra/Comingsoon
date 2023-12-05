5ce625c1cf58949b5ad4e6615bbe5c4c065d51a0
Parmjeet <parmjeetmishra2@gmail.com>
2023-12-06 00:03:52 +0530

    some

--- 00PortfolioMain/src/App.jsx
+++ 00PortfolioMain/src/App.jsx
@@ -1,15 +1,34 @@
 import { Navbar, Sidebar } from './components/index.js'
+import dark from './assets/dark.svg'
 
 const App = () => {
   return (
     <>
-      <div className='container grid grid-cols-12 md:gap-10 justify-between lg:mt-[220px]'>
-        {/* Sidebar */}
-        <div className='col-span-12 lg:col-span-4 lg:h-screen lg:sticky top-44'>
-          <Sidebar />
+      <section className='bg-homeBg min-h-screen bg-no-repeat bg-center bg-cover bg-fixed dark:bg-homeTwoBg-dark md:pb-16 w-full'>
+        <div className='container w-full bg-[#F3F6F6] dark:bg-black lg:bg-transparent lg:dark:bg-transparent flex justify-between py-5 lg:px-0 lg:pt-[50px]'>
+          <div className='w-full flex justify-between px-4'>
+            <a href='#home'>Parmjeet Mishra</a>
+            <div className='flex items-center'>
+              <span className='bg-white w-[40px] hover:text-white hidden h-[40px] rounded-full lg:flex justify-center items-center text-black hover:bg-[#ef4060] transition-all duration-300 ease-in-out cursor-pointer ml-2 '>
+                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="text-3xl dark-mode-light dark:hidden" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="text-2xl dark-mode-dark hidden fill-jacarta-700 group-hover:fill-white group-focus:fill-white dark:block dark:fill-white " height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
+              </span>
+              <span className='lg:opacity-0 lg:invisible visible opacity-100 bg-[#ef4060] w-[40px] h-[40px] rounded-full flex justify-center cursor-pointer items-center text-white dark:text-white text-3xl ml-3 '>
+               <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path></svg>
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
-    <Navbar />
+        <div className='container grid grid-cols-12 md:gap-10 justify-between lg:mt-[220px]'>
+          {/* Sidebar */}
+          <div className='col-span-12 lg:col-span-4 lg:h-screen lg:sticky top-44'>
+            <Sidebar />
+          </div>
+          <div className='col-span-12 lg:col-span-8 '>
+            <Navbar />
+          </div>
+        </div>
+      </section>
     </>
   )
 }
