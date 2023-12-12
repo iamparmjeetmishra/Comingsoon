76f114268b8ef36bed681478148a484f7951f82b
Parmjeet <parmjeetmishra2@gmail.com>
2023-12-12 23:50:09 +0530

    Minor Updates

--- 00PortfolioMain/src/components/AboutCard.jsx
+++ 00PortfolioMain/src/components/AboutCard.jsx
@@ -1,7 +1,15 @@
 
-const AboutCard = () => {
+
+
+const AboutCard = (imgUrl, title, desc) => {
   return (
-    <div>AboutCard</div>
+    <div className="about-box dark:bg-transparent" >
+      <img alt="icon" src={imgUrl} width="40" height="40" decoding="async" data-nimg="1" className="w-10 h-10 object-contain block" loading="lazy" />
+      <div className="space-y-2 break-all">
+        <h3 className="dark:text-white text-xl font-semibold">{ title }</h3>
+        <p className=" leading-8 text-gray-lite dark:text-[#A6A6A6]">{ desc } </p>
+      </div>
+    </div>
   )
 }
 
