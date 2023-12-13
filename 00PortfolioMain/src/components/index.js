3b3da04b728d7c28fab7b70514a360422c2968bb
Parmjeet <parmjeetmishra2@gmail.com>
2023-12-13 23:52:55 +0530

    Folder deleted and new start

--- 00PortfolioMain/src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Navbar from "./Navbar";
-import Sidebar from './Sidebar'
-import Button from "./ui/Button";
-import AboutCard from './AboutCard'
-
-
-export  { Navbar, Sidebar, Button, AboutCard }
